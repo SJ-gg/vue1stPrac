70df1f2745b5a33b7e92b2ce65e86776780d38b5
SJ-gg <sesin0126.kang@gmail.com>
2025-06-22 21:14:19 +0900

    7강-상세페이지 모달 추가 및 데이터 구조 변경

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
+
+  <div class="black-bg" v-if="모달창열렸니">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="a in 메뉴들" :key="a"> {{ a }} </a>
   </div>
   <div v-for="(products, i) in products" :key="i">
-    <img :src="`./assets/room${i}.jpg`" class="room-img">
-    <h4>{{ products }} 원룸</h4>
-    <p>50 만원</p>
+    <img :src="products.image" alt="room image" class="room-img">
+    <h4 @click="모달창열렸니 = true">{{ products.title }} 원룸</h4>
+    <p>{{ products.price }} 만원</p>
     <button @click="increase(i)">허위매물신고</button>
-    <span>신고수 : {{ 신고수[i] }}</span>
+    <span> 신고수 : {{ 신고수[i] }}</span>
   </div>
 </template>
 
 <script>
+import data from './assets/oneroom.js'
+
 export default {
   name : 'App',
   data() { // 모든 변수 데이터
     return {
+      products : data,
+      모달창열렸니 : false,
       신고수 : [0, 0, 0],
       메뉴들 : ['Home', 'Shop', 'About'],
-      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
   },
   methods: {
@@ -32,6 +44,22 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -48,5 +76,9 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
