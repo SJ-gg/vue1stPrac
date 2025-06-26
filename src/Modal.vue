70c8e2f3c60f10fe6b5d3529427b7df1a2080347
SJ-gg <sesin0126.kang@gmail.com>
2025-06-26 20:31:09 +0900

    13강까지 완(숙제포함)

--- src/Modal.vue
+++ src/Modal.vue
@@ -4,8 +4,8 @@
       <h4>{{ products[productsClick].title }}</h4>
       <img :src="products[productsClick].image" alt="room image" class="room-img" style="width: 100%">
       <p>{{ products[productsClick].content }}</p>
-      <input @input="month = $event.target.value">
-      <p> {{ month }} 개월 선택함 : {{ products[productsClick].price }} 원</p>
+      <input v-model.number="month">
+      <p> {{ month }}개월 선택함 : {{ products[productsClick].price * month}} 원</p>
       <button @click="closeModal()">닫기</button>
     </div>
   </div>
@@ -17,6 +17,20 @@ export default {
   data() {
     return {
       month: 1,
+    }
+  },
+  watch: {
+    month(monthNum) {
+      if (monthNum === '') return;
+      if (
+        typeof monthNum !== 'number' ||
+        isNaN(this.month) ||
+        monthNum < 0 ||
+        monthNum > 12
+      ) {
+        alert('1~12 사이의 숫자만 입력할 수 있습니다.');
+        this.month = 1;
+      }
     }
   },
   props: {
